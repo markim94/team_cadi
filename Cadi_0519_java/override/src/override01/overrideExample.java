18eb7bfc6f93d03e63f30af577f157fc75086402
markim94 <markim94@naver.com>
2018-05-21 11:43:27 +0900

    added override project remark

--- Cadi_0519_java/override/src/override01/overrideExample.java
+++ Cadi_0519_java/override/src/override01/overrideExample.java
@@ -1,25 +1,24 @@
 package override01;
 	
-	
-class SuperClass{
-	public void itis() {
-		System.out.println("It's super class");
+class SuperClass{ 			// 부모 클래스
+	public void itis() { 	// 부모클래스의 itis 메소드 
+		System.out.println("It's super class"); // it's super class 출력 
 	}
 }
 
-class SubClass extends SuperClass{
-	public void itis() {
-		System.out.println("It's sub class");
+class SubClass extends SuperClass{  // 부모 클래스를 상속(extends)해서 만든 자식 클래스
+	public void itis() {			// 자식 클래스의 itis 메소드, 부모 클래스의 itis 메소드를 재정의(오버라이딩).
+		System.out.println("It's sub class"); // it's sub class 출력
 	}
 }
 
-class overrideExample {
-	public static void main(String[] args) {
-		SuperClass sc = new SuperClass();
-		SubClass bc = new SubClass();
+class overrideExample { 			// 재정의한 메소드를 확인하고자 만든 메인 클래스
+	public static void main(String[] args) { 
+		SuperClass sc = new SuperClass(); // 부모 클래스를 sc로 객체화함.
+		SubClass bc = new SubClass(); 	  // 자식 클래스를 bc로 객체화함.
 		
-		sc.itis();
-		bc.itis();
+		sc.itis();		//부모 클래스의 itis메소드 호출
+		bc.itis();		//자식 클래스의 itis메소드 호출
 	}
 }
 
