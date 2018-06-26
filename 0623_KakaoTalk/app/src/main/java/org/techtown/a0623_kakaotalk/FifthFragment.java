8c3f9d9004b2b8946daac8153c35ad12f5a4876b
markim94 <markim94@naver.com>
2018-06-26 23:48:11 +0900

    add listview

---
+++ 0623_KakaoTalk/app/src/main/java/org/techtown/a0623_kakaotalk/FifthFragment.java
@@ -0,0 +1,34 @@
+package org.techtown.a0623_kakaotalk;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+
+public class FifthFragment extends Fragment {
+
+    public FifthFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fifth, container, false);
+    }
+
+
+
+}
--- 0623_KakaoTalk/app/src/main/java/org/techtown/a0623_kakaotalk/MainActivity.java
+++ 0623_KakaoTalk/app/src/main/java/org/techtown/a0623_kakaotalk/MainActivity.java
@@ -8,8 +8,16 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ListView;
+
+import java.lang.reflect.GenericArrayType;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView = null;
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.search_tab, menu);
@@ -21,23 +29,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tl_tabs);
         //viewpager
         ViewPager viewPager = (ViewPager) findViewById(R.id.vp_pager);
 
-        //프래그먼트 배열 - 만들어 놓은 프래그먼트를 차례대로 넣어 준다.
-        Fragment[] arrFragments = new Fragment[3];
+        //프래그먼트 배열 - 만들어 놓은 프래그먼트를 차례대로 넣음.
+        Fragment[] arrFragments = new Fragment[5];
         arrFragments[0] = new FirstFragment();
         arrFragments[1] = new SecondFragment();
         arrFragments[2] = new ThirdFragment();
+        arrFragments[3] = new FourthFragment();
+        arrFragments[4] = new FifthFragment();
 
-        //어답터 생성후 연결 - 배열을 인자로 추가해 준다.
+        //어답터 생성후 연결 - 배열을 인자로 추가.
         MyPagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager(), arrFragments);
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
     }
 
-    //pager adapter class(빨간줄에서 Alt+Enter키로 '구현')
+
+
+
+
+    //pager adapter
     private class MyPagerAdapter extends FragmentPagerAdapter {
 
         private Fragment[] arrFragments;
@@ -47,6 +62,9 @@ public class MainActivity extends AppCompatActivity {
             super(fm);
             this.arrFragments = arrFragments;
         }
+
+
+
 
         @Override
         public Fragment getItem(int position) {
@@ -58,16 +76,22 @@ public class MainActivity extends AppCompatActivity {
             return arrFragments.length;
         }
 
+
+
         //Tab의 타이틀 설정
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "First";
+                    return "친구";
                 case 1:
-                    return "Second";
+                    return "대화";
                 case 2:
-                    return "Third";
+                    return "뉴스";
+                case 3:
+                    return "게임";
+                case 4:
+                    return "더보기";
                 default:
                     return "";
             }
