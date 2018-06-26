e03322741be74491c781e9e914e6285bd68eef97
markim94 <markim94@naver.com>
2018-06-26 20:48:10 +0900

    add tab_menu.xml

--- 0623_KakaoTalk/app/src/main/java/org/techtown/a0623_kakaotalk/MainActivity.java
+++ 0623_KakaoTalk/app/src/main/java/org/techtown/a0623_kakaotalk/MainActivity.java
@@ -2,6 +2,7 @@ package org.techtown.a0623_kakaotalk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public boolean onCreateOptionMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.search_tab, menu);
+        return true;
     }
 }
