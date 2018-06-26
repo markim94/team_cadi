f7f004965aa5a8d27c4632d9956cd4cb7b7a90de
markim94 <markim94@naver.com>
2018-06-26 22:43:02 +0900

    added viewpager

--- 0623_KakaoTalk/app/src/main/java/org/techtown/a0623_kakaotalk/MainActivity.java
+++ 0623_KakaoTalk/app/src/main/java/org/techtown/a0623_kakaotalk/MainActivity.java
@@ -1,14 +1,15 @@
 package org.techtown.a0623_kakaotalk;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
-    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.search_tab, menu);
@@ -20,7 +21,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tl_tabs);
+        //viewpager
+        ViewPager viewPager = (ViewPager) findViewById(R.id.vp_pager);
+
+        //프래그먼트 배열 - 만들어 놓은 프래그먼트를 차례대로 넣어 준다.
+        Fragment[] arrFragments = new Fragment[3];
+        arrFragments[0] = new FirstFragment();
+        arrFragments[1] = new SecondFragment();
+        arrFragments[2] = new ThirdFragment();
+
+        //어답터 생성후 연결 - 배열을 인자로 추가해 준다.
+        MyPagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager(), arrFragments);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
+
+    //pager adapter class(빨간줄에서 Alt+Enter키로 '구현')
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+
+        private Fragment[] arrFragments;
+
+        //생성자
+        public MyPagerAdapter(FragmentManager fm, Fragment[] arrFragments) {
+            super(fm);
+            this.arrFragments = arrFragments;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return arrFragments[position];
+        }
+
+        @Override
+        public int getCount() {
+            return arrFragments.length;
+        }
+
+        //Tab의 타이틀 설정
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "First";
+                case 1:
+                    return "Second";
+                case 2:
+                    return "Third";
+                default:
+                    return "";
+            }
+            //return super.getPageTitle(position);
+        }
+    }
+
+
 
 
 }
