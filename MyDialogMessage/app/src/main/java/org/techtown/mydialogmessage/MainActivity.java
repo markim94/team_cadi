508fc43af7373ab9d5a4c53c144e546548790868
markim94 <markim94@naver.com>
2018-07-11 21:20:07 +0900

    add comment for dialog message

--- MyDialogMessage/app/src/main/java/org/techtown/mydialogmessage/MainActivity.java
+++ MyDialogMessage/app/src/main/java/org/techtown/mydialogmessage/MainActivity.java
@@ -16,39 +16,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        //버튼 다이얼로그 메시지
+        // 다이얼로그 메시지 생성
         AlertDialog.Builder newBuilder = new AlertDialog.Builder(this);
-        newBuilder.setTitle("오늘 수업을 끝낼까요?");
-        newBuilder.setMessage("오늘 수업을 끝내시겠습니까?");
+
+        // 다이얼로그 메시지 타이틀 설정
+        newBuilder.setTitle("어플리케이션을 종료여부");
+
+        // 다이얼로그 메시지 본문 내용 설정
+        newBuilder.setMessage("어플리케이션을 종료하시겠습니까?");
+
+        // 다이얼로그 메시지 icon 설정
+        newBuilder.setIcon(R.drawable.icon);
+
         newBuilder.setNegativeButton("예", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                // '예' 버튼 클릭
                 finish();
             }
         });
+
         newBuilder.setPositiveButton("아니요", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                // '아니요' 버튼 클릭
                 Toast.makeText(getApplicationContext(), "끝내지 않습니다.", Toast.LENGTH_SHORT).show();
             }
-
         });
 
+        // '취소' 버튼 클릭
+        newBuilder.setNeutralButton("취소", null);
 
-        // AlertDialog의 Builder를 이용해 정의한 newBuilder를 dialog
+
+        // AlertDialog.Builder를 이용해 정의한 newBuilder를 dialog에 적용
         final AlertDialog dialog = newBuilder.create();
 
-
-        // 버튼 클릭시 dialog message를 show함.
+        // 버튼 클릭시 dialog message를 show함
         Button button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 dialog.show();
-
             }
         });
-
     }
 }
