3615fe1aec33d43d36c83c704e1015159a53d001
markim94 <markim94@naver.com>
2018-08-04 15:51:49 +0900

    modified 0804

---
+++ 0804_Bluetooth/app/src/main/java/org/markim/a0804_bluetooth/BluetoothListActivity.java
@@ -0,0 +1,13 @@
+package org.markim.a0804_bluetooth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BluetoothListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_list);
+    }
+}
--- 0804_Bluetooth/app/src/main/java/org/markim/a0804_bluetooth/MainActivity.java
+++ 0804_Bluetooth/app/src/main/java/org/markim/a0804_bluetooth/MainActivity.java
@@ -1,7 +1,10 @@
 package org.markim.a0804_bluetooth;
 
 import android.Manifest;
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,11 +17,8 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
     // bluetooth adapter
     BluetoothAdapter bluetoothAdapter = null;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +32,13 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                checkMyPermission();
             }
         });
     }
 
+
+    // 권한 체크 메소드, Ted Permission 라이브러리를 이용.
     private void checkMyPermission(){
         TedPermission.with(this)
                 .setRationaleMessage("정상적인 앱 사용을 위해서 블루투스 권한을 허용해주시기 바랍니다.")
@@ -51,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
 
     // TedPermission 라이브러리 내에 존재하는 Permission Listener
     PermissionListener permissionListener = new PermissionListener(){
-
 
         // 권한 허가를 정상적으로 받았을 때
         @Override
@@ -76,9 +77,40 @@ public class MainActivity extends AppCompatActivity {
         //null이 return되면 블루투스가 사용불가 상태임.
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        //권한 허용 라이브러리 : Ted Permission implementation
+        // bluetooth Adapter가 null이 아니고 사용가능하다면
+        if(bluetoothAdapter.isEnabled()){
 
+            Intent i = new Intent(MainActivity.this, BluetoothListActivity.class);
+            startActivity(i);
+            finish();
+
+            // null이라면 응답코드를 던져서 블루투스를 키게 됨.
+        } else {
+            Intent bluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            // 10이상으로 정의하면 됨. 통상 100
+            startActivityForResult(bluetoothIntent, 100);
+        }
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode)
+        {
+            case 100:
+                // 요청코드 10을 던지고 resultCode가 정상적으로 반환됐을때
+                if(resultCode==Activity.RESULT_OK){
+                    // BluetoothListActivity로 전환
+                    Intent i = new Intent(MainActivity.this, BluetoothListActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+                else{
+                    // 활성화되지 않았을 때 코드부분
+                }
+        }
+    }
 
 }
